ad290e11d922d67e13982880a9fc80b68fe63813
cezarlofellem@gmail.com <cezarlofellem@gmail.com>
2023-09-29 17:06:40 -0700

    updated results and logo file placement

--- src/components/Results.js
+++ src/components/Results.js
@@ -3,16 +3,22 @@ import Meaning from "./Meaning";
 import "./Results.css";
 
 export default function Results({ results }) {
-  console.log(results);
   if (results) {
     return (
       <div className="Results">
         <section>
           <h2> {results.word}</h2>
           <div>
-            <Meaning meaning={results.meanings[2]} /> <br />
-            <Meaning meaning={results.meanings[3]} /> <br />
-            <Meaning meaning={results.meanings[1]} /> <br />
+            {results.meanings[2] && <Meaning meaning={results.meanings[2]} />}{" "}
+            <br />
+            {results.meanings[3] && (
+              <Meaning meaning={results.meanings[3]} />
+            )}{" "}
+            <br />
+            {results.meanings[1] && (
+              <Meaning meaning={results.meanings[1]} />
+            )}{" "}
+            <br />
           </div>
         </section>
       </div>
